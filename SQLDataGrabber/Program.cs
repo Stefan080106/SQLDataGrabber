04287bb9d53cfa520485ba327c17e5c4b50fdade
stefan080106 <stefan08.arnold@gmail.com>
2022-09-16 23:14:21 +0200

    Projektdateien hinzufügen.

---
+++ SQLDataGrabber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.SqlClient;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string connetionString;
+        SqlConnection cnn;
+        connetionString = @"Server=localhost;Database=master;Trusted_Connection=True;";
+        cnn = new SqlConnection(connetionString);
+        cnn.Open();
+        Console.WriteLine("Connection Open  !");
+
+        SqlCommand command;
+        SqlDataReader dataReader;
+        string sql, Out = "";
+
+        sql = "SELECT * FROM Test";
+
+        command = new SqlCommand(sql, cnn);
+
+        dataReader = command.ExecuteReader();
+
+        while (dataReader.Read())
+        {
+            Out = Out + dataReader.GetValue(0) + " - " + dataReader.GetValue(1) + "\n";
+        }
+
+        Console.WriteLine(Out);
+
+        cnn.Close();
+    }
+}
